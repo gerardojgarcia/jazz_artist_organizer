0bec1ed5d5d76de16801a6f28b812eef563a8027
Gerardo Garcia <gerardojgarcia@pm.me>
2023-07-07 22:30:59 -0700

    fist commit

---
+++ src/main.ts
@@ -0,0 +1,42 @@
+
+interface JazzAlbum {
+  albumName: string
+  artistName: string
+  date: string
+  recordLabel: string
+}
+
+
+function createJazzAlbum(albumName: string, artistName: string, date: string, recordLabel: string):JazzAlbum {
+  const jazzAlbum: JazzAlbum = {
+    albumName,
+    artistName,
+    date,
+    recordLabel
+  }
+
+
+  return jazzAlbum
+}
+
+
+
+function saveJazzAlbumToLocalStorage(jazzAlbum: JazzAlbum): void {
+  const jsonData = JSON.stringify(jazzAlbum);
+  localStorage.setItem('jazzAlbum', jsonData);
+}
+
+
+function getJazzAlbumFromLocalStorage(): JazzAlbum | null {
+  const jsonData = localStorage.getItem('jazzAlbum');
+  if (jsonData) {
+    return JSON.parse(jsonData);
+  }
+  return null;
+}
+
+
+
+const newAlbum = createJazzAlbum("Kind of blue", "Miles Davis", "1959", "Columbia Records")
+
+console.log(newAlbum)
